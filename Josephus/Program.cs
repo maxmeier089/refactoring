473671f023dfe81519dd46af3eaed34a29d59112
Max Meier <hello@maxmeier.net>
2022-09-26 20:01:26 +0200

    Add project files.

---
+++ Josephus/Program.cs
@@ -0,0 +1,10 @@
+﻿using Josephus;
+
+Console.WriteLine(JosephusBad.GetSolution(8));
+Console.WriteLine();
+
+Console.WriteLine(JosephusBad.GetSolution(41));
+Console.WriteLine();
+
+Console.WriteLine(JosephusBad.GetSolution(82));
+Console.WriteLine();
